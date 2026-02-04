29dcc609eed2c04b8ea036ba8877a0720002398c
AmahleZuma <170299648+AmahleZuma@users.noreply.github.com>
2026-02-04 10:57:20 +0200

    Making changes to CSS layout

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,44 @@ function App() {
       {/* Contains the quiz */}
       <div className='app-container'>
 
-        {/* Image container. Images should change */}
-        <div className='image'>
-
+        <div className='score-container'>
+          <p className='score'>20/55</p>
         </div>
 
-        <div className='option'>Option 1</div>
-        <div className='option'>Option 2</div>
-        <div className='option'>Option3</div>
+        <div className='question-container'>
+          <p className='question'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos dolor quo fuga, quaerat eveniet obcaecati itaque modi, voluptas maiores, minima nemo perferendis dolorum quod! Sapiente officiis dolorem omnis accusamus aspernatur?</p>
+        </div>
+
+        {/* Image container. Images should change */}
+        <div className='image-container'>
+          <img className='image' src="" alt="" />
+        </div>
+
+        <div className='option-container'>
+          <p className='option'>
+            Option 1
+          </p>
+        </div>
+
+        <div className='option-container'>
+          <p className='option'>
+            Option 2
+          </p>
+        </div>
+
+        <div className='option-container'>
+          <p className='option'>
+            Option 3
+          </p>
+        </div>
+
+        <div className='control-container'>
+          <button type="submit">SKIP</button>
+          <button type="submit">
+            ➡️
+          </button>
+        </div>
+
 
 
 
